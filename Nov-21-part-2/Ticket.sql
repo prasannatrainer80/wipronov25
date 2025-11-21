ae354aa894c105801f8b85240445a34a043bd46c
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-11-21 15:50:56 +0530

    code

--- Nov-21-part-2/Ticket.sql
+++ Nov-21-part-2/Ticket.sql
@@ -5,13 +5,13 @@ create table Ticket
     Id INT Primary Key auto_increment,
     title varchar(30),
     description varchar(30),
-    customerName varchar(30),
-    customerMobile varchar(30),
+    customer_Name varchar(30),
+    customer_Mobile varchar(30),
     status varchar(30)
 );
 
-Insert into Ticket(Title,Description,CustomerName,
-customerMobile,status) values('Lets C','C Language Book',
+Insert into Ticket(Title,Description,customer_Name,
+customer_Mobile,status) values('Lets C','C Language Book',
 'Vatsal','848284845','open'),
 ('Java Programming','Simon Copy','Satya','9772344','closed'),
 ('Java Black Book','Hudson','Swetha','884823445','open')
