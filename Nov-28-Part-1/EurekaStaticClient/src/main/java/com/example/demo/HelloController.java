caa71078f1602a78519e44f856e8f17ab3fe0450
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-11-28 09:39:40 +0530

    code

---
+++ Nov-28-Part-1/EurekaStaticClient/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping(value="/")
+	public String hello() {
+		return "Hello World...";
+	}
+	
+	@GetMapping("/service")
+	public String service() {
+		return "Welcome to Micro-Services";
+	}
+}
