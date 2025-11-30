cc3bb28cb8b2f47b6c086b48ffa91252ca46aae0
prasannatrainer80 <prasanna.trainer.java@gmail.com>
2025-11-30 15:47:44 +0530

    code

---
+++ Nov-30-Part-2/MailCode/src/main/java/com/java/mail/App.java
@@ -0,0 +1,10 @@
+package com.java.mail;
+
+public class App {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    MailSend.sendMail("iamprakashsatya707@gmail.com", "Java Program Mail", "Welcome this is First Mail By Program...");
+  }
+
+}
